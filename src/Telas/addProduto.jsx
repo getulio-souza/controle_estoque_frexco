44fd71c2b749091cb2c40c70319bec9904fb81ca
Getúlio Souza <getuliosouza@live.com>
2022-05-27 13:48:05 -0300

    construindo o componente de formulário

--- src/Telas/addProduto.jsx
+++ src/Telas/addProduto.jsx
@@ -1,24 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from "@material-ui/core/Button";
 import AddIcon from "@material-ui/core/Icon";
- const addProduto = () => {
-  return <>
+import { Container, CssBaseline, Typography } from '@material-ui/core';
+
+const addProduto = () => {
+
+  const [input, setInput] = useState("maça");
+  
+   return <>
+     <CssBaseline/>
     <div className='main_div'>
       <div className='center_div'>
-        <img className='background_img' src="https://www.frexco.com.br/_next/static/media/logo.27ca8367.png"/>
-        <br />
-        <h1>Preencha sua lista de compras </h1>
-        <br />
-        <input type="text" placeholder='Adicione o alimento desejado' className='input_produto' />
-        {/* lista de alimentos */}
-        <article className='lista_alimentos'>
-        <div className='campo_alimento'>campo 1</div>
-        <div className='campo_alimento'>campo 2</div>
-        <div className='campo_alimento'>campo 3</div>
-        </article>
-        <Button className="newBtn">
-          <AddIcon/>
+        {/* logo */}
+        <img className='logo' src="https://www.frexco.com.br/_next/static/media/logo.27ca8367.png"/>
+         <br />
+         <Container>
+           <Typography variant='h4' align='center' gutterBottom>
+            Preencha sua lista de compras
+           </Typography>
+        </Container>
+         <br />
+         
+        <div className='search_box'>
+        <input type="text" placeholder='Adicione o alimento desejado' className='input_produto'
+        />
+        <Button variant='contained' color='default'>
+        +
         </Button>
+         </div>
+         
+        {/* lista de alimentos */}
+         <Container maxWidth="sm">
+        <ul className='lista_alimentos'>
+        <li className='campo_alimento'>campo 1</li>
+        <li className='campo_alimento'>campo 2</li>
+        <li className='campo_alimento'>campo 3</li>
+        </ul>
+        </Container>
+        <Button variant='contained' color='default'>Checar estoque</Button>
       </div>
   </div>
   </>
